6d5f0d7ca098fa1248bd1051099c8e4e48d31641
Fausto Soares <fausto.ifs@gmail.com>
2024-05-04 17:52:24 -0300

    Commit inicial

---
+++ src/main/java/br/com/estacio/todo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package br.com.estacio.todo.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class IndexController {
+
+    @GetMapping
+    public String index(){
+        return "Minha Lista";
+    }
+
+}
